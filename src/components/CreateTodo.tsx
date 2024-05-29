95f76ab82179d53707a703c1c1e1da8d8b85e71b
ChoiJaHyun <dngel9880@gmail.com>
2024-05-29 11:52:49 +0900

    first commit

---
+++ src/components/CreateTodo.tsx
@@ -0,0 +1,15 @@
+import { Button, Flex, Input } from "@chakra-ui/react";
+import { FC } from "react";
+
+const CreateTodo: FC = () => {
+  return (
+    <Flex bgColor="teal.200" h={32} justifyContent="center" alignItems="center">
+      <Input />
+      <Button ml={2} colorScheme="teal">
+        만들기
+      </Button>
+    </Flex>
+  );
+};
+
+export default CreateTodo;
